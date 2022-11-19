a84cbab791b03f5d7aa31fede5701d3c74e8251d
Beatrice Olatunbosun <beatriceola95@hotmail.com>
2022-11-19 15:48:22 +0000

    comments removed

--- js/index.js
+++ js/index.js
@@ -20,11 +20,9 @@ function modalContainer(targetDate) {
     });
 }
 
-
 for (let i = 0; i < dates.length; i++) {
   const timelineItem = document.createElement("div");
   timelineItem.className = "timeline-item";
-
 
   const modalButton = document.createElement("button");
   modalButton.className = "timeline-item-more-info";
@@ -42,51 +40,10 @@ for (let i = 0; i < dates.length; i++) {
   document.querySelector(".timeline").appendChild(timelineItem);
 }
 
-// function ourButton () {
-//   const modalButton = document.createElement("button");
-//   modalButton.className = "timeline-item-more-info";
-//   modalButton.textContent = "More Info";
-
-//   function openModal () {
-//   const modalDiv = document.createElement("div");
-//   modalDiv.className = "modal-container";
-//   modalDiv.innerHTML = `<span id ="modal-date"> ${dates[i].date}`
-//   }
-
-//   ourButton.onclick = () => openModal(modalDiv);
-//   document.querySelector(".timeline").appendChild(ourButton);
-// }
-
 const fullModalDescription = dates;
-
-
-
-
-
-//dates.map((fullModalDescription, dates) => {
-// const modalBox = document.createElement("div");
-// const buttonOpen = document.createElement("button")
-// const ourDate = document.createElement("span");
-// const ourTitle = document.createElement("h2");
-// const ourImage = document.createElement("img");
-// const ourDescription = document.createElement("p");
-// const buttonClose = document.createElement("span");
-
-// modalBox.setAttribute("id", "modal-container");
-// buttonOpen.setAttribute("id", "timeline-item-more-info")
-// ourDate.setAttribute("id","modal-date");
-// ourTitle.setAttribute("id", "modal-title");
-// ourDate.setAttribute("id", "modal-image");
-// ourDescription.setAttribute("id", "modal-full-description");
-// buttonClose.setAttribute("id", "modal-close-button");
 
 const modalBox = document.createElement("div");
 modalBox.setAttribute("id", "modal-container");
 document.querySelector(".timeline").appendChild(modalBox);
-
-
-//buttonOpen.onclick
-
-//})
 
 console.log(fullModalDescription);
